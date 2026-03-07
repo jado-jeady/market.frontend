510aa5a4d1dbdbd0b6708c7bea37c2dbd5576d09
jado-jeady <jeadynzaramyimana@gmail.com>
2026-03-07 09:21:38 +0200

    fixing case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import Login from "./Pages/Login";
+import Login from "./pages/Login";
 import { AdminRoutes } from "./routes/AdminRoutes";
 import { UserRoutes } from "./routes/UserRoutes";
 import { StorekeeperRoutes } from "./routes/StoreKeeperRoutes";
